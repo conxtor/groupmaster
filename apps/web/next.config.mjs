cf950b242d968d47e3b16b6fdd57684d28fcf4e8
Volker Kerkhoff <volker@kerkhoff.es>
2026-08-12 15:24:36 +0200

    Einige änderungen für Prodiktivbetrieb

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "standalone" };
+const nextConfig = {
+  output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.API_INTERNAL_URL || "http://api:8080"}/api/:path*`,
+      },
+    ];
+  },
+};
 export default nextConfig;
